379ff6aababbc39a13278a08c9c2d30b7385cb58
congerian <congerian@yandex.ru>
2021-08-11 00:01:55 +0300

    Добавил базовые исключения

---
+++ Core/src/ru/arcein/plugins/arceinultimate/core/module/exception/WrongModuleConfigException.java
@@ -0,0 +1,9 @@
+package ru.arcein.plugins.arceinultimate.core.module.exception;
+
+public class WrongModuleConfigException extends ModuleLoadException{
+
+    public WrongModuleConfigException(String message){
+        super(message);
+    }
+
+}
